ef777608bc1df46c7d3d95f4fc53171cc18d56fb
P Sai Ramcharan Reddy <89081036+RandomArtist22@users.noreply.github.com>
2025-10-17 13:29:04 +0530

    Change WebSocket URL to ngrok endpointin
    Updated WebSocket server URL to a new endpoint.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import Canvas from './components/Canvas';
 import Controls from './components/Controls';
 import { PencilIcon } from './components/Icons';
 
-const WS_URL = import.meta.env.VITE_WEBSOCKET_URL; // WebSocket server URL
+const WS_URL = 'wss://dorathy-conceptional-yawnfully.ngrok-free.dev' ; // WebSocket server URL
 
 const App: React.FC = () => {
   const [pixels, setPixels] = useState<string[]>([]);
